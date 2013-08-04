ec74b035ca53a4b33c95400032d76841931d5ef9
regtan <regtan1210@gmail.com>
2013-08-04 18:22:33 +0900

    update footer

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :branch, "master"
 set :user, "regtan"
 
 role :web, "kirino.org"
-set :deploy_to, '/home/regtan/tmp'
+set :deploy_to, '/var/www/public_html/staging.kirino.org'
 
 namespace :deploy do
 end
